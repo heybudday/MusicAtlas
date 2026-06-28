b8bf1987ce60db4aabfb51a6dfccdcf15c089126
Yuri Wuensch <yuri@enigmamachine.ca>
2026-06-28 11:35:58 -0600

    MA-098 Add ExitCommand registration

--- tests/test_exit_command.py
+++ tests/test_exit_command.py
@@ -1,7 +1,13 @@
-from app.ui.exit_command import ExitCommand
+from app.ui.command import Command
+from app.ui.default_commands import register_default_commands
+from app.ui.command_registry import CommandRegistry
 
 
-def test_exit_command_returns_false():
-    command = ExitCommand()
+def test_exit_command_is_registered():
+    registry = CommandRegistry()
 
-    assert command.execute() is False
+    register_default_commands(registry)
+
+    command = registry.get("exit")
+
+    assert isinstance(command, Command)
